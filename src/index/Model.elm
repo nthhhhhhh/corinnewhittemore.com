5c5e4fd395cccb6e3e23fa1eaf29c18c4f243d6d
joseph <j.sph@me.com>
2018-06-16 19:46:04 -0500

    contains pagetype to indentify currently displayed content beneath navbar and hero.

--- src/index/Model.elm
+++ src/index/Model.elm
@@ -1,16 +1,28 @@
 module Model exposing (Model, init)
 
-type alias Model = 
+import Messages exposing (Msg)
+import Html exposing (Html)
+import Home exposing (content)
+import PageType exposing (PageType(..))
+
+
+type alias Model =
     { isBurgerActive : Bool
-    , isModalActive  : Bool
+    , isModalActive : Bool
+    , content : Html Msg
+    , pageType : PageType
     }
+
 
 initialModel : Model
 initialModel =
     { isBurgerActive = False
-    , isModalActive  = False
+    , isModalActive = False
+    , content = Home.content
+    , pageType = Index
     }
 
-init : (Model, Cmd msg)
+
+init : ( Model, Cmd msg )
 init =
-    (initialModel, Cmd.none)
+    ( initialModel, Cmd.none )
