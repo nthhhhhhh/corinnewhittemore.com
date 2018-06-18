3e57645c84ea44b5167ddabf65f268ae8d6be2f8
joseph <j.sph@me.com>
2018-06-18 03:34:38 -0500

    Main content now loaded by Elm Navigation.

--- src/index/Index.elm
+++ src/index/Index.elm
@@ -1,17 +1,18 @@
 module Index exposing (..)
 
-import Hero exposing (section_)
-import Html exposing (div, Html, text)
-import Html.Attributes exposing (class)
+import Html exposing (Html, text, div, a)
+import Html.Attributes exposing (class, href)
 import Model exposing (Model, init)
 import Messages exposing (Msg(..))
+import PageContent exposing (pageContent)
+import Navigation
 import NavBar exposing (navBar)
 import Update exposing (update)
 
 
 main : Program Never Model Msg
 main =
-    Html.program
+    Navigation.program UrlChange
         { init = init
         , view = view
         , update = update
@@ -27,8 +28,8 @@ view : Model -> Html Msg
 view model =
     div [ class "main-container" ]
         [ navBar model.isBurgerActive "./images/design/ecmw_black.png"
-        , Hero.section_ <| model.heroTitle
-        , model.content
+        , model.hero
+        , pageContent model
         ]
 
 
