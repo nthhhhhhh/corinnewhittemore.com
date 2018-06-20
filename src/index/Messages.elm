ee53d6d6ac60272608841db615c18be3ca07b3d4
joseph <j.sph@me.com>
2018-06-19 19:57:01 -0500

    Contentchange and PageType are replaced by UrlChange and Navigation

--- src/index/Messages.elm
+++ src/index/Messages.elm
@@ -2,12 +2,6 @@ module Messages exposing (Msg(..))
 
 import Keyboard
 import Navigation
-import PageType exposing (PageType(..))
-
-
--- WARN
-{--contentchange and pagetype should be replaced by
-  - urlchange and navigation.location respectively --}
 
 
 type Msg
@@ -17,5 +11,4 @@ type Msg
     | KeyMsg Keyboard.KeyCode
     | Burger
     | ImageClick Int
-    | ContentChange PageType
     | UrlChange Navigation.Location
