e55e51bae9f94b64b63f299ae81beea8975bca4e
joseph <j.sph@me.com>
2018-06-19 19:58:43 -0500

    Refactored for elm Navigation

--- src/index/Model.elm
+++ src/index/Model.elm
@@ -5,6 +5,7 @@ import Messages exposing (Msg)
 import Html exposing (Html, section)
 import Home exposing (content)
 import PageType exposing (PageType(..))
+import Routing exposing (Route)
 
 
 type alias Model =
@@ -14,19 +15,24 @@ type alias Model =
     , pageType : PageType
     , heroTitle : Maybe { title : String, subtitle : String }
     , hero : Html Msg
-    , currentRoute : Navigation.Location
+    , route : Route
+    }
+
+
+initialModel : Route -> Model
+initialModel route =
+    { isBurgerActive = False
+    , isModalActive = False
+    , content = Home.content
+    , pageType = HomePage
+    , heroTitle = Nothing
+    , hero = section [] []
+    , route = route
     }
 
 
 init : Navigation.Location -> ( Model, Cmd msg )
 init location =
-    ( { isBurgerActive = False
-      , isModalActive = False
-      , content = Home.content
-      , pageType = HomePage
-      , heroTitle = Nothing
-      , hero = section [] []
-      , currentRoute = location
-      }
+    ( initialModel <| Routing.parseLocation location
     , Cmd.none
     )
