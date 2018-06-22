a85a5310f6ad88a620a58ac0ec1db98a83805e61
joseph <j.sph@me.com>
2018-06-22 04:27:17 -0500

    design one project 3 finished

---
+++ src/studentwork/1_ARTS1311-DesignOne/DesignOne3.elm
@@ -0,0 +1,88 @@
+module DesignOne3 exposing (content)
+
+import HtmlUtils exposing (AltString)
+import Html exposing (Html, article, section, text, div, h2, p)
+import Html.Attributes exposing (class, style)
+import ImageDirectory exposing (designOneImages, paths, resolutions, titles)
+import Messages exposing (Msg)
+import StudentWorkUtil exposing (imageTiles, margins, tabs, underline)
+
+
+divTileDescription : Html Msg
+divTileDescription =
+    div [ class "tile is-parent is-6" ]
+        [ article [ class "content" ]
+            [ p [ class "title is-3 has-text-weight-bold" ] [ text "The Project" ]
+            , p [ class "is-6" ] [ text """Compose four 6x6 inch black and
+            white images using the initials of your name.""" ]
+            ]
+        ]
+
+
+divTileDescription2 : Html Msg
+divTileDescription2 =
+    div [ class "tile is-parent is-6" ]
+        [ article [ class "content" ]
+            [ h2 [ class "title is-3 has-text-weight-bold" ]
+                [ text "The Elements" ]
+            , p [ class "is-6" ]
+                [ text """Plate 1: Asymmetrical with 5 values of grays (can include black and white). Plate 2: Symmetrical/Radial composition or Disrupted Symmetry. Plate 3: Asymmetrical with area of emphasis (contrast, isolation, placement). Plate 4: Crystallographic Balance (all over pattern)"""
+                ]
+            ]
+        ]
+
+
+divTileStudentImagesRow1 : Html Msg
+divTileStudentImagesRow1 =
+    let
+        students =
+            [ "3 1-1", "3 4-4", "3 6-1" ]
+    in
+        div [ class "tile" ] <|
+            imageTiles
+                (titles designOneImages students)
+                (paths designOneImages students)
+                (resolutions designOneImages students)
+
+
+divTileStudentImagesRow2 : Html Msg
+divTileStudentImagesRow2 =
+    let
+        students =
+            [ "3 12-3", "3 14-3", "3 16" ]
+    in
+        div [ class "tile" ] <|
+            imageTiles
+                (titles designOneImages students)
+                (paths designOneImages students)
+                (resolutions designOneImages students)
+
+
+divTileStudentImages : Html Msg
+divTileStudentImages =
+    div [ class "tile is-parent is-vertical" ]
+        [ divTileStudentImagesRow1
+        , divTileStudentImagesRow2
+        ]
+
+
+divTileIsAncestor : Html Msg
+divTileIsAncestor =
+    div [ class "tile is-ancestor is-vertical", style [ ( "padding-top", "3%" ) ] ]
+        [ div [ class "tile is-parent" ]
+            [ divTileDescription
+            , divTileDescription2
+            ]
+        , divTileStudentImages
+        ]
+
+
+content : Int -> Html Msg
+content i =
+    section
+        [ margins
+        ]
+        [ tabs i
+        , underline
+        , divTileIsAncestor
+        ]
