026b377081be6ba1e64b8951c5ceeba96635e39a
joseph <j.sph@me.com>
2018-06-22 01:45:34 -0500

    assign content for new routes

--- src/index/PageContent.elm
+++ src/index/PageContent.elm
@@ -4,11 +4,13 @@ import Home exposing (content)
 import Html exposing (Html, div, text)
 import ImportantPapers exposing (content)
 import ItalyJournals exposing (content)
+import DesignOne1 exposing (content)
 import Messages exposing (Msg)
 import Navigation
+import NotFound exposing (content)
 import Routing exposing (Route(..), route, parseLocation)
-import UrlParser exposing (parseHash)
 import PrivateDisturbance exposing (content)
+import TeachingPhilosophy exposing (content)
 import ValleyCultura exposing (content)
 
 
@@ -23,22 +25,54 @@ page location =
                 "valley_cultura" ->
                     ValleyCultura.content
 
+                "important_papers" ->
+                    ImportantPapers.content
+
+                "italy_journals" ->
+                    ItalyJournals.content
+
+                "private_disturbance" ->
+                    PrivateDisturbance.content
+
                 _ ->
-                    div [] []
+                    NotFound.content
 
         DesignRoute ->
-            div [] []
+            text "Design"
 
         HomeRoute ->
             Home.content
 
-        StudentWorkRoute str ->
+        StudentWorkRoute str i ->
             case str of
                 "teaching_philosophy" ->
-                    div [] []
+                    TeachingPhilosophy.content
+
+                "design_one" ->
+                    case i of
+                        1 ->
+                            DesignOne1.content i
+
+                        _ ->
+                            text "Design One"
+
+                "design_two" ->
+                    case i of
+                        _ ->
+                            text "Design Two"
+
+                "digital_media" ->
+                    case i of
+                        _ ->
+                            text "Digital Media"
+
+                "independent_study" ->
+                    case i of
+                        _ ->
+                            text "Independent Study"
 
                 _ ->
-                    div [] []
+                    NotFound.content
 
         NotFoundRoute ->
-            text "Not Found"
+            NotFound.content
